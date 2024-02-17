2ff8ccbeac3f295d52d84f8ca69c18993ea30095
cejimenez0123 <christianjimenez0123@gmail.com>
2024-02-17 11:42:28 -0500

    remove bark

--- routes/fork.js
+++ routes/fork.js
@@ -1,20 +1,62 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const prisma = require("../db")
+const prisma = require("../db");
 
 const router = express.Router()
 
 module.exports = function(authMiddleware){
-    router.post("/fork",authMiddleware, async function(req, res){
-        const {task,type} = req.body;
-        const user = req.user
-        if(user){
-            await prisma.fork.create({data: {path:task,user: user,task: task,type:type}})
-        }
-    })
-    router.get("/fork", async function(req, res){
 
-    })
+    router.post("/",authMiddleware,async (req,res)=>{
+            const {fork,task}=req.body
+            const user = req.user
+            const newFork = await prisma.fork.create({
+                data: {
+                    name: task,
+                    user:{
+                        connect:{
+                            id: user.id
+                        }
+                    },
+                    parent:{
+                        connect:{
+                            id: fork.id
+                        }
+                    }
+                }})
+
+         res.json(newFork)
+        })
+
+        router.get("/",async (req,res)=>{
+            const fork = await prisma.fork.findUnique({where:{
+                id: "65ce6f093ed66e8a5da96c07"
+            }})
+            res.json(fork)
+
+        })
+        router.get("/children/:id", async (req,res)=>{
+            const {id} = req.params
+            const forks = await prisma.fork.findMany({where:{
+               AND: {
+                    parentId: id,
+                }
+                    
+            }})
+            res.json(forks)
+        })
+        router.get("/protected/children/:id",authMiddleware,async (req, res) => {
+
+            const {id} = req.params
+            const forks = await prisma.fork.findMany({
+                where: {
+                    AND:{
+                        parentId: id,
+                        userId: req.user.id
+                    }
+                }})
+            res.json(forks)
+
+        })
     return router
 }
