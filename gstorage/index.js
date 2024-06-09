9b18ec2e6761a324d61a6e71dc5dbb4a19ea37f7
cejimenez0123 <christianjimenez0123@gmail.com>
2024-06-09 16:18:48 -0400

    keyfilename

--- gstorage/index.js
+++ gstorage/index.js
@@ -1,6 +1,6 @@
 const { Storage } = require('@google-cloud/storage');
 
 // Create a Storage client instance
-const storage = new Storage({projectId:process.env.PROJECT_ID});
+const storage = new Storage({projectId:process.env.PROJECT_ID,keyFilename:`../${process.env.keyFilename}`});
 
 module.exports = storage.bucket(process.env.BUCKET_NAME)
