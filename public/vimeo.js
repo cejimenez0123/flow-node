1a4c9a224c5a5f20d9c389bea2a1ec08348b1f75
cejimenez0123 <christianjimenez0123@gmail.com>
2024-08-06 10:38:57 -0400

    vimeo

--- public/vimeo.js
+++ public/vimeo.js
@@ -6,6 +6,8 @@ const vimeo = [
     "https://vimeo.com/874474957",
     "https://vimeo.com/741193833",
     "https://vimeo.com/520521742",
-    "https://vimeo.com/442856328"
+    "https://vimeo.com/442856328",
+    "https://www.youtube.com/watch?v=0ScYz9sNaQk",
+    "https://www.youtube.com/watch?v=NhheiPTdZCw"
 ]
 module.exports = vimeo
