f561cb374bd5662855068cd85ff13841636270bf
cejimenez0123 <christianjimenez0123@gmail.com>
2024-08-05 20:19:07 -0400

    viemo video

---
+++ twilio/index.js
@@ -0,0 +1,14 @@
+require('dotenv')
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+18444223429',
+        to: '+18777804236'
+   })
+  .then(message => console.log(message));
+
+  module.exports = client
